6426e8b4a7595d10435a18e349f5f67735bbef47
JorgeMaiconFaria <jorgemaicon.faria@gmail.com>
2022-10-16 14:16:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+const pokemonName = document.querySelector('.pokemon_name');
+
 const fetchPokemon = async (pokemon) =>{
-    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon${pokemon}`)
+    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIresponse.json();
+    return data
+}
+
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+
 }
