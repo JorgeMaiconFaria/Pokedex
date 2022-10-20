4175692ef0a87e858c12254601a7837d3d88127e
JorgeMaiconFaria <jorgemaicon.faria@gmail.com>
2022-10-20 10:34:46 -0300

    Update script.js
    Final version

--- js/script.js
+++ js/script.js
@@ -2,18 +2,55 @@ const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_img');
 
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+
+const buttonNext = document.querySelector('.button-Next');
+const buttonPrev = document.querySelector('.button-Prev');
+
+let searchPokemon = 1
+
+
 const fetchPokemon = async (pokemon) =>{
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    
+    if (APIresponse.status == 200){
     const data = await APIresponse.json();
-    return data
-}
+    return data}
+} 
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML ='Loading...'
+    pokemonNumber.innerHTML = ''
+
     const data = await fetchPokemon(pokemon);
+
+    if(data){    
+        pokemonImage.style.display = 'block';
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value ='';
+    searchPokemon = data.id;
 
+    } else {pokemonName.innerHTML = 'Not found :c'
+            pokemonNumber.innerHTML = ''
+            pokemonImage.style.display = 'none'
+}
 }
 
-renderPokemon('pichu')
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1
+    renderPokemon(searchPokemon)
+});
+buttonPrev.addEventListener('click', () => {
+    if(searchPokemon > 1) {
+    searchPokemon -= 1
+    renderPokemon(searchPokemon)
+}});
+    
+renderPokemon(searchPokemon);
